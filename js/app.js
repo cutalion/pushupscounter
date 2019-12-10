dd0d725e232bf7af57cf1180d1c3bbf77668c08a
Alexander Glushkov <cutalion@gmail.com>
2019-12-10 22:43:29 +0300

    Styling

--- js/app.js
+++ js/app.js
@@ -5,3 +5,35 @@ if('serviceWorker' in navigator) {
 } else {
   console.log('serviceWorker is not supported');
 }
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  var counter = document.getElementById('counter');
+  var count   = document.getElementById('count');
+  var plus    = document.getElementById('plus');
+  var minus   = document.getElementById('minus');
+
+  plus.addEventListener('click', (e) => {
+    let total = parseInt(counter.innerText)
+    let amount = parseInt(count.value)
+
+    if (isNaN(amount)) {
+      console.log('Amount is NaN');
+      return;
+    }
+
+    counter.innerText = total + amount;
+  })
+
+  minus.addEventListener('click', (e) => {
+    let total = parseInt(counter.innerText)
+    let amount = parseInt(count.value)
+
+    if (isNaN(amount)) {
+      console.log('Amount is NaN');
+      return;
+    }
+
+    let result = total - amount
+    counter.innerText = result < 0 ? 0 : result;
+  })
+});
